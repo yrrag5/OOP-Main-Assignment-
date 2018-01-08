91666f4c03d6ba1f174bf64761775f03fbb857c0
yrrag5 <G00335806@gmit.ie>
2018-01-08 18:27:51 +0000

    Adding runner(menu needs to be implemented) and shingle clases

---
+++ Workspace/JavaAPI/src/ie/gmit/oop/Runner.java
@@ -0,0 +1,9 @@
+package ie.gmit.oop;
+
+import java.awt.Menu;
+
+public class Runner {
+	public static void main(String f1, String f2) {
+		new Menu().show();
+	}
+}
---
+++ Workspace/JavaAPI/src/ie/gmit/oop/Shingle.java
@@ -0,0 +1,22 @@
+// Author: Garry Cummins
+// OOP Project
+package ie.gmit.oop;
+
+public class Shingle {
+	private int docId;
+	private int hashcode;
+	//Constructor + Getters/Setters
+	public int getDocId() {
+		return docId;
+	}
+	public void setDocId(int docId) {
+		this.docId = docId;
+	}
+	public int getHashcode() {
+		return hashcode;
+	}
+	public void setHashcode(int hashcode) {
+		this.hashcode = hashcode;
+	}
+
+}
